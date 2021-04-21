9a6bae0a31850544b6b52a500492dd4cff2ebe77
lou <dustyfingers01257@gmail.com>
2021-04-21 09:06:01 -0600

    completed step 1-4 of boss challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(),
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(
+        child: Ball(),
+      ),
+      backgroundColor: Colors.blue.shade600,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        print('I got clicked!');
+      },
+      child: Image(
+        image: AssetImage('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
